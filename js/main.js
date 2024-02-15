83bc14877608e7670f645bc407b88d8237de0309
josgonmor <josan4452@gmail.com>
2024-02-15 22:51:26 +0100

    Mejorando las imagenes

--- js/main.js
+++ js/main.js
@@ -2,12 +2,21 @@ function loadEventsIndex(divContenedor, divRepetir, eventos){
     divContenedor.innerHtml = ''
     var repetido;
     var num = 0;
+    var descField
     for(e of eventos){
+        descField = ""
         repetido = divRepetir.cloneNode(true)
-        repetido.querySelector('.imagen').style.backgroundImage = "url(images/imagesEvents/"+num+".jpg)"
+        repetido.querySelector('.imagen').style.backgroundImage = "url("+e.image+")"
         num++
         repetido.querySelector('.titulo').innerText = e.event
-        repetido.querySelector('.texto').innerText = e.description
+        if(e.official){
+            repetido.querySelector('.titulo').innerHTML+= '<img src="images/check.png" style="height: 20px"></img>'
+        }
+        for(let i = 0; i<35;i++){
+            descField+=e.description[i]
+        }
+        descField+="..."
+        repetido.querySelector('.texto').innerText = descField
         repetido.querySelector('.autor').innerText = e.creator
         divContenedor.appendChild(repetido)
         console.log(repetido)
